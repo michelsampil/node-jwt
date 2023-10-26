44fd20e51bbee3be16eb12904ef1993d2d5c664f
michel sampil <michel.sampil@gmail.com>
2023-10-25 22:29:30 -0300

    fixing evn

--- src/app.ts
+++ src/app.ts
@@ -3,15 +3,9 @@ import express from "express";
 import bodyParser from "body-parser";
 import loginRouter from "./routes/login";
 import protectedRouter from "./routes/protected";
-import dotenv from "dotenv";
 
 const app = express();
-const port = process.env.PORT || 3000;
-const secretKey = "littleSecret";
-
-console.log("secretKey", secretKey);
-
-dotenv.config();
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 app.use("/api", loginRouter);
